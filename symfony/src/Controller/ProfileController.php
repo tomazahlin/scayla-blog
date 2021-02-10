10fb0de32b7a6e2d4dba6e6d2da41cbdb92d80e2
Nick Steinwand <nick.steinwand@docu.ch>
2021-02-10 20:57:06 +0100

    prepare profile settings

--- symfony/src/Controller/ProfileController.php
+++ symfony/src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserSettingsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +16,28 @@ class ProfileController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/profile.html.twig', ['value' => 'profile page']);
+        return $this->render('home/profile.html.twig', ['user' => $this->getUser()]);
     }
+
+    /**
+     * @Route("/secure/profile/settings", name="app_secure_settings")
+     */
+    public function settings(Request $request): Response
+    {
+        $userSettingsForm = $this->createForm(UserSettingsType::class, $this->getUser());
+
+        $userSettingsForm->handleRequest($request);
+
+        if ($userSettingsForm->isSubmitted() && $userSettingsForm->isValid()) {
+            $userData = $userSettingsForm->getData();
+            dump($userData);
+            $userRepo = $this->getDoctrine()->getRepository(User::class);
+            $userRepo->save($userData);
+        }
+
+        return $this->render('home/profile_settings.html.twig', [
+            'userSettingsForm' => $userSettingsForm->createView(),
+        ]);
+    }
+
 }
